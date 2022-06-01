1aa631b9892fdaf263b893a4bb1adaf8a8f5d65b
Shelby Neuman <shelbydneuman@gmail.com>
2022-05-31 20:07:51 -0600

    working

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import path from 'path'
-// import plugin from path.resolve(__dirname, './src/plugin.ts')
-import POV from 'my-awesome-plugin';
+// import POV from 'point-of-vue';
+import pointofvue from '../node_modules/point-of-vue/dist/index.js'
 import './styles.css'
 const app = createApp(App);
-app.use(plugin);
+app.use(pointofvue);
 app.use(router).mount('#app')
 
 // app.mount('#app')
